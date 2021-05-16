b4cb2a15f981cb66994ae071ed6f09659f05f503
Kira060200 <constantin.gab02@gmail.com>
2021-05-16 19:42:43 +0300

    improved 4-heuristic 22s

--- placi_suprapuse.py
+++ placi_suprapuse.py
@@ -374,11 +374,11 @@ class Graph:  # graful problemei
                     if '*' == infoNod[i][j] and not(i < len(infoNod) - 1 and infoNod[i + 1] == '*'):
                         steps += len(infoNod) - 1 - i
                         diff += 1
-                        if i < col - 1:
-
+                        if i < lin - 1:
+                            '''
                             st = j - 1
                             dr = j + 1
-                            # presupunem ca exista spatiu sa mutam una din placile invecinate cu bila noastra
+                            # presupunem ca exista spatiu sa mutam una din placile de jos invecinate cu bila noastra
                             ok = True
                             while st > 0 and infoNod[i + 1][st - 1] == infoNod[i][st]:
                                 st -= 1
@@ -391,37 +391,39 @@ class Graph:  # graful problemei
                             # daca placile nu pot fi mutate in laterala si avem o placa cu lungime pana la capete mai mare decat 1 in ambele directii de la bila
                             if not ok and (j - 1 < 0 or infoNod[i + 1][j - 1] == infoNod[i + 1][j]) and (j + 1 >= col or infoNod[i + 1][j + 1] == infoNod[i + 1][j]):
                                 steps += 1
-                                '''
+                            '''
                             st = j
                             dr = j
-                            # presupunem ca nu exista spatiu sa mutam una din placile de jos invecinate cu bila noastra
-                            ok1 = False
-                            cost_min = 1
+                            ok = False
+                            # daca exista posibilitatea ca bila sa fie impinsa pe langa una din placile inferioare
+                            if (j + 1 < col and infoNod[i + 1][j + 1] == '.') or (j - 1 >= 0 and infoNod[i + 1][j - 1] == '.'):
+                                ok = True
                             while st > 0 and infoNod[i + 1][st - 1] == infoNod[i + 1][st]:
                                 st -= 1
-                            if i > 0 and st > 0 and infoNod[i + 1][st - 1] == '.':
-                                ok1 = True
+                            # daca pot sa mut placa din stanga inferioara astfel incat sa fac loc caderii bilei
+                            if i > 0 and st > 0 and infoNod[i + 1][st - 1] == '.' and (j + 1 >= col or infoNod[i + 1][j + 1] != infoNod[i + 1][j]):
+                                ok = True
                             while dr < col - 1 and infoNod[i + 1][dr + 1] == infoNod[i + 1][dr]:
                                 dr += 1
-                            if i > 0 and dr < col - 1 and infoNod[i + 1][dr + 1] == '.':
-                                ok1 = True
+                            # daca pot sa mut placa din dreapta inferioara astfel incat sa fac loc caderii bilei
+                            if i > 0 and dr < col - 1 and infoNod[i + 1][dr + 1] == '.' and (j - 1 < 0 or infoNod[i + 1][j - 1] != infoNod[i + 1][j]):
+                                ok = True
 
-                            # presupunem ca nu exista spatiu sa mutam una din placile invecinate cu bila noastra
-                            ok2 = False
-                            while st > 0 and infoNod[i][st - 1] == infoNod[i][st]:
-                                st -= 1
-                            if i > 0 and st > 0 and infoNod[i][st - 1] == '.':
-                                ok2 = True
-                                cost_min = j - st + 2
-                            while dr < col - 1 and infoNod[i][dr + 1] == infoNod[i][dr]:
-                                dr += 1
-                            if i > 0 and dr < col - 1 and infoNod[i][dr + 1] == '.':
-                                ok2 = True
-                                cost_min = min(j - st + 2, cost_min)
-                            # daca placile nu pot fi mutate in laterala si avem o placa cu lungime pana la capete mai mare decat 1 in ambele directii de la bila
-                            if not ok1 and ok2 and (j - 1 < 0 or infoNod[i + 1][j - 1] != '.') and (j + 1 >= col or infoNod[i + 1][j + 1] != '.'):
+                            if not ok:
+                                cost_min = min(2 + dr - j, 2 + j - st)
+                                st = j
+                                dr = j
+                                # presupunem ca nu exista spatiu sa mutam una din placile invecinate cu bila noastra
+                                while st > 0 and infoNod[i][st - 1] == infoNod[i][st]:
+                                    st -= 1
+                                if i > 0 and st > 0 and infoNod[i][st - 1] == '.':
+                                    cost_min = min(cost_min, j - st + 2)
+                                while dr < col - 1 and infoNod[i][dr + 1] == infoNod[i][dr]:
+                                    dr += 1
+                                if i > 0 and dr < col - 1 and infoNod[i][dr + 1] == '.':
+                                    cost_min = min(dr - j + 2, cost_min)
                                 steps += cost_min - 1
-                                '''
+
                         if diff > 1:
                             st = j - 1
                             dr = j + 1
